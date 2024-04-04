da08389f8ec62c9c1fc359e8d7252d25c5e98292
RICCOM <ericomun@gmail.comgit>
2024-04-04 15:38:42 +0300

    adds dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+const mainElement = document.querySelector('#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = "Eric is the champion";
+
